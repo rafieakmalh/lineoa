acf1790040d52d3be8b8bc80af9c70de54465f40
rafieakmalh <74704272+rafieakmalh@users.noreply.github.com>
2021-04-04 03:54:08 +0700

    Add files via upload

--- botni/app.py
+++ botni/app.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
 from __future__ import unicode_literals
 
 import errno
 import os
 import sys
 import tempfile
+import time, random, json, null, codecs, html5lib ,shutil ,threading, glob, re, base64, string, os, requests, six, ast, pytz, wikipedia, urllib, urllib.parse, atexit, asyncio, traceback, pafy, sys, timeago, argparse
 from argparse import ArgumentParser
 
 from flask import Flask, request, abort
@@ -52,6 +40,10 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
 
 # function for create tmp dir for download content
+def removeCmd(cmd, text):
+	rmv = len(key + cmd) + 1
+	return text[rmv:]
+
 def make_static_tmp_dir():
     try:
         os.makedirs(static_tmp_path)
@@ -64,26 +56,21 @@ def make_static_tmp_dir():
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    text = event.message.text
-
+    text = event.message.text #simplify for receove message
+    sender = event.source.user_id #get user_id
+    gid = event.source.sender_id #get group_id
     if text == 'profile':
         if isinstance(event.source, SourceUser):
             profile = line_bot_api.get_profile(event.source.user_id)
@@ -101,6 +88,9 @@ def handle_text_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextMessage(text="Bot can't use profile API without user ID"))
+    elif text == 'joox':
+        hasil = removeCmd("joox", text)
+        print(hasil)
     elif text == 'bye':
         if isinstance(event.source, SourceGroup):
             line_bot_api.reply_message(
